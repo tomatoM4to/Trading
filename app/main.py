510b4e532a196c78b594716fe6df9ff983f71086
tomatoM4to <tomatomato2121@gmail.com>
2026-08-06 19:48:18 +0900

    update: CORS

--- app/main.py
+++ app/main.py
@@ -86,7 +86,11 @@ app = FastAPI(
 # Source: https://fastapi.tiangolo.com/tutorial/cors/
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "https://trading-one-kappa.vercel.app"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
